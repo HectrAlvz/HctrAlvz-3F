df1712f3d4d846de7f144222e4f0e7adc6250c76
Hector Antonio <TheSoityn_Mx@outlook.com>
2022-11-22 10:29:06 -0600

    1recursive

--- 3p/1recursive/2do/2.0problems.js
+++ 3p/1recursive/2do/2.0problems.js
@@ -24,3 +24,58 @@
 //
 // call
 // factorial(3) returns 6
+
+// 1. fibonacci | fibonacci(5) returns the 5 first numbers of the series
+function fibonacci(n) {
+if (n === 1) {
+        console.log(1)
+    } else if (n === 2) {
+        console.log(1)
+        console.log(1)
+
+    } else {
+        console.log(1)
+        console.log(1)
+        let a = 1
+        let b = 1
+        for (let i = 3; i <= n; i++) {
+            let c = a + b
+            console.log(c)
+            a = b
+            b = c
+        }
+    }
+    return ''
+}
+
+console.log("fibonacci")
+console.log(fibonacci(5))
+
+console.log(" ")
+// 2. power | power(2,3) returns 8
+function power(b, p) {
+    let c = 1
+    for (let i = 1; i <= p; i++) {
+        c = c * b
+    }
+    return c
+}
+
+console.log("power")
+console.log(power(2, 3))
+
+console.log(" ")
+
+// 3. factorial | factorial(3) returns 6
+function factorial(n) {
+    if (n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+console.log("\nfactorial")
+console.log(factorial(3))
+
+console.log(" ")
